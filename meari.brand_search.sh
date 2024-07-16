4d098fb4039fc379b2aca0dc0d017c9fcf0772b8
Flobul <46993341+Flobul@users.noreply.github.com>
2024-07-16 18:10:46 +0200

    Update script

--- meari.brand_search.sh
+++ meari.brand_search.sh
@@ -3,29 +3,32 @@
 #already known brands
 array="1 8 36 39 51 55 62 65 67 75 77 80 81 82 84 86 92 93 96 97 98 100 102 104 107 108 109 112 114 115 119 122 124 129 131 135"
 
-for ((i=1 ;i<200 ; i++))
-do
-  resultA=$(curl -s -o /dev/null -w "%{http_code}"  "https://apis-eu-frankfurt.cloudedge360.com/img/$i/en/Google_Assistant.html" | xargs)
-  if (( "$resultA" == "200")); then
-    if ! echo "$array" | grep -w -q "$i"; then
-      echo "FOUND GOOGLE ASSISTANT ===> $i: code $resultA"
-      curl "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/userAgreement/$i/en/Agreement_en.html" | grep "item-title"
-    fi
-  fi
+# Fonction pour vérifier si un nombre est dans la liste des exclus
+is_excluded() {
+  local num="$1"
+  echo "$array" | grep -w -q "$num"
+}
 
-  resultB=$(curl -s -o /dev/null -w "%{http_code}"  "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/userAgreement/$i/en/Agreement_en.html" | xargs)
-  if (( "$resultB" == "200")); then
-    if ! echo "$array" | grep -w -q "$i"; then
-      echo "FOUND AGREEMENT ===> $i: code $resultB"
-      curl "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/userAgreement/$i/en/Agreement_en.html" | grep "Service Agreement of"
-    fi
-  fi
+# Fonction pour traiter une URL
+process_url() {
+  local url="$1"
+  local label="$2"
+  local num="$3"
+  local grep_pattern="$4"
 
-  resultC=$(curl -s -o /dev/null -w "%{http_code}"  "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/privacyAgreement/$i/en/Privacy.html" | xargs)
-  if (( "$resultC" == "200")); then
-    if ! echo "$array" | grep -w -q "$i"; then
-      echo "FOUND PRIVACY ===> $i: code $resultC"
-      curl "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/privacyAgreement/$i/en/Privacy.html" | grep "Service Agreement of"
+  local result=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+  if (( "$result" == "200" )); then
+    if ! is_excluded "$num"; then
+      echo "FOUND $label ===> $num: code $result"
+      curl "$url" | grep "$grep_pattern"
     fi
   fi
+}
+
+# Boucle principale
+for ((i=1; i<200; i++)); do
+  process_url "https://apis-eu-frankfurt.cloudedge360.com/img/$i/en/Google_Assistant.html" "GOOGLE ASSISTANT" "$i" "item-title"
+  process_url "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/userAgreement/$i/en/Agreement_en.html" "AGREEMENT" "$i" "Service Agreement of"
+  process_url "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/privacyAgreement/$i/en/Privacy.html" "PRIVACY" "$i" "Service Agreement of"
 done
