171db88353caf599495bee4c9fa8c7b6fd11fdee
Flobul <46993341+Flobul@users.noreply.github.com>
2024-08-15 15:25:26 +0200

    rename json file

--- get_brands.sh
+++ get_brands.sh
@@ -9,6 +9,7 @@ is_excluded() {
   echo "$array" | grep -w -q "$num"
 }
 
+# Initialisation de la variable JSON
 json_result="{"
 
 # Fonction pour traiter une URL
@@ -20,27 +21,40 @@ process_url() {
 
   local result=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
+  # Debug: Affiche le code HTTP
+  echo "HTTP code for $url: $result"
+
   if (( "$result" == "200" )); then
     if ! is_excluded "$num"; then
-      if echo "$json_result" | grep -q "\"$num\""; then
-        json_result=$(echo "$json_result" | sed "s/\"$num\":{/\"$num\":{ \"$label\"/")
-      else
-        if [[ "$json_result" != "{" ]]; then
-          json_result+=", "
+      # Debug: Vérifie le contenu de la page
+      if curl -s "$url" | grep -q "$grep_pattern"; then
+        echo "$label found in $url"
+        if echo "$json_result" | grep -q "\"$num\""; then
+          json_result=$(echo "$json_result" | sed "s/\"$num\":{/\"$num\":{ \"$label\"/")
+        else
+          if [[ "$json_result" != "{" ]]; then
+            json_result+=", "
+          fi
+          json_result+="\"$num\": \"$label\""
         fi
-        json_result+="\"$num\": \"$label\""
+      else
+        echo "Pattern '$grep_pattern' not found in $url"
       fi
     fi
   fi
 }
 
 # Boucle principale
-for ((i=1; i<200; i++)); do
+#for ((i=1; i<200; i++)); do
+for i in {1..200}; do
   process_url "https://apis-eu-frankfurt.cloudedge360.com/img/$i/en/Google_Assistant.html" "GOOGLE ASSISTANT" "$i" "item-title"
   process_url "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/userAgreement/$i/en/Agreement_en.html" "AGREEMENT" "$i" "Service Agreement of"
-  process_url "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/privacyAgreement/$i/en/Privacy.html" "PRIVACY" "$i" "Service Agreement of"
+  process_url "https://static-eus.s3.eu-central-1.amazonaws.com/common/app/privacyAgreement/$i/en/Privacy.html" "PRIVACY" "$i" "Privacy"
 done
 
+# Fermeture du JSON
 json_result+="}"
+
+# Affichage du JSON
 echo "$json_result"
-echo "${json_result}" > src/new_brands.json
+echo "${json_result}" > src/brands.json
