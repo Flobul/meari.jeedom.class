8ed3a5eaf4735a04225171f71c333d4f0e487636
Flobul <46993341+Flobul@users.noreply.github.com>
2026-03-28 10:44:36 +0100

    Bump version

--- src/Capabilities.php
+++ src/Capabilities.php
@@ -4,7 +4,7 @@ namespace Flobul\Meari;
 
 class Capabilities
 {
-    public static $version = "1.014";
+    public static $version = "1.015";
     public $enums;
     public $apps;
     public $errors;
